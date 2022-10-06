4663ac74f3324cbd13743a24bcb6e28527079ac9
Netanel Alroy <netanelal100@gmail.com>
2022-10-06 19:19:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import Message from './comps/message';
+
 import './App.css';
-import Counter from './comps/counter';
-import InputVal from './comps/inputVal';
+
 
 function App() {
   return (
     <div className="container">
       <h1 className='text-danger'>React</h1>
-      {/* חיבור בין קומפנינטה
-      פשוט זימון שלה ממש כמו תגית יחידה */}
-      {/* <Message txt="first 111111" /> */}
-      {/* 
-      let msg1 = new Message("first 1111")
-      msg1.render()
-      */}
-      {/* <Message txt="last 22222"  /> */}
-      <Counter />
-      <InputVal />
+ 
     </div>
   );
 }
