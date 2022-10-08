fbc2b01f0981b832a10527c8b845b5401c35d436
Netanel Alroy <netanelal100@gmail.com>
2022-10-08 21:14:58 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import { AppApi } from './components/apiCopms/appApi';
+import AppExchange from './components/appExchange';
+import AppInput from './components/appInput';
+import AppParent from './components/appParent';
+import Counter from './components/counter';
+import AppDate from './components/dateComps/appDate';
 
 
 function App() {
   return (
-    <div className="container">
-      <h1 className='text-danger'>React</h1>
+    <div className="container m-auto mx-auto">
+   
+      {/* <AppDate/> */}
+      {/* <AppParent/> */}
+      {/* <AppExchange/> */}
+      {/* <AppInput/> */}
+{/* <Counter/> */}
  
     </div>
   );
