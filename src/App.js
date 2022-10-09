07960ea6e61fa67805cfc84a2e143641546aeb80
Netanel Alroy <netanelal100@gmail.com>
2022-10-09 16:23:16 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-
+import React from "react";
+import react from "react";
 import './App.css';
-import { AppApi } from './components/apiCopms/appApi';
-import AppExchange from './components/appExchange';
-import AppInput from './components/appInput';
-import AppParent from './components/appParent';
-import Counter from './components/counter';
-import AppDate from './components/dateComps/appDate';
+
 
 
 function App() {
   return (
-    <div className="container m-auto mx-auto">
-   
-      {/* <AppDate/> */}
-      {/* <AppParent/> */}
-      {/* <AppExchange/> */}
-      {/* <AppInput/> */}
-{/* <Counter/> */}
- 
-    </div>
+  <React.Fragment>
+    react
+  </React.Fragment>
   );
 }
 
