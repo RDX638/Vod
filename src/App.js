47bfe5d571456af893a7f4429f35ffd84746770e
Netanel Alroy <netanelal100@gmail.com>
2022-10-25 17:40:07 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import AppAtlas from "./atlas_comps/appAtlas";
-import Crypto from "./components/crypto";
-import Movie from "./components/movie";
+
 function App() {
   return (
     <div>
-      {/* <Crypto /> */}
-      {/* <Movie /> */}
-      <AppAtlas/>
+  
     </div>
   );
 }
