720cbeec26e45385069eddfbcb2b2e4652a5ae0d
Netanel Alroy <netanelal100@gmail.com>
2022-11-03 19:10:07 +0200

    a

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 function App() {
   return (
     <React.Fragment>
-      <div className='text-danger'>
+      <div className='text-danger text h1 display-1 center container'>
      react
       </div>
     </React.Fragment>
