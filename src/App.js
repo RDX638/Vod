b2622980a65b3f230559a8dea9dce17f5f6f23c5
Netanel Alroy <netanelal100@gmail.com>
2022-11-08 17:27:21 +0200

    React setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
 import React from 'react';
 
 
-function App() {
+const App =() => {
   return (
     <React.Fragment>
       <div className='text-danger text h1 display-1 center container'>
